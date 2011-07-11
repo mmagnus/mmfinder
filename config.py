18e0259222fda93d840a72fc9f98240cc8d23b80
Magnus <mag_dex@o2.pl>
2011-07-11 21:54:41 +0200

    add file:

---
+++ config.py
@@ -0,0 +1,19 @@
+RUN_UPDATE = True
+
+LOCS = {
+    'EON'     : '/media/EONVECTOR',
+    #1 SD 'EON'     : '/media/EONVECTOR',
+    #2 SD 
+
+    'SD8GB'   : '/media/SD8GB',
+    'SD8GB2'   : '/media/SD8GB2'    ,
+
+    #'maximus' : '/home/magnus/',
+    #'1TB'     : '/media/1TB',
+    }
+
+LOCS_NETBOOK = {
+    'netbook' : '/home/magnus/',
+    }
+
+PATH_DB = '/home/magnus/Dropbox/workspace/mmfinder/db/'
